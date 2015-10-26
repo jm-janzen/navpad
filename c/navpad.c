dc48a3f9a1e3fd2c0140d040aceab77a71795d24
JM <jmjanzen@gmail.com>
2015-10-26 10:58:12 -0400

    Experimental Commit, to be reverted

--- c/navpad.c
+++ c/navpad.c
@@ -6,14 +6,20 @@ int main(void)
 {
     DIR *d;
     struct dirent *dir;
+    char list[50];
+    int ctr = 0;
+
     d = opendir(".");
+
     if (d) {
         while ((dir = readdir(d)) != NULL) {
-            if (dir->d_name != "..") {
-                printf("%s\n", dir->d_name);
-            }
+            printf("%s\n", dir->d_name);
+            list[ctr] = (char) dir->d_name;
         }
         closedir(d);
+    }
+    for (int i = 0; i < sizeof(list)/sizeof(int); i++) {
+        printf("%s", list[i]);
     }
 
     getchar();
