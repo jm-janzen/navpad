ead3f5bad6fc1f1f35bb74595abd959523cbcc8c
JM <jmjanzen@gmail.com>
2015-12-22 01:58:40 +1000

    Allow for crude backwards nav & error if cannot open directory & comments

--- navpad.c
+++ navpad.c
@@ -4,7 +4,8 @@
 
 /* XXX bugs:
  * 1)   entering N greater than sizeof num files: segmentation fault
- * 2)   
+ * 2)   entering directory typeof int: segmentation fault
+ * 3)   attempting to read file (not dir): segmentation fault
  */
  
 /*
@@ -36,6 +37,7 @@ void prompt() {
     switch (answer) {
         case (0):
             printf("going back\n");
+            read("../");
             break;
         default:
             printf("default\n");
@@ -51,26 +53,31 @@ void prompt() {
  */
 void read(char* l) {
     DIR* d = opendir(l);
-    if (d) {
-        int idx = 0;
-        while ((dir = readdir(d)) != NULL) {
-            char* fileName = dir->d_name;
-            if (!strcmp(fileName, "..") || !strcmp(fileName, ".")) {
-                /*
-                 * supress display of ./ ../
-                 */
-            } else if (!strncmp(".", fileName, 1)) {
-                /*
-                 * suppress display of hidden files
-                 */
-            } else {
-                idx++; // start at [1], [0] reserved for prev dir
-                dirStack[idx] = fileName;
-                printf("[%d]\t%s\n", idx, dirStack[idx]);
-            }
-        }
-        closedir(d);
+    if (d == NULL) {
+        printf("Error! Could not open directory %s", l);
     }
+
+    int idx = 0;
+    while ((dir = readdir(d)) != NULL) {
+        char* fileName = dir->d_name;
+        if (!strcmp(fileName, "..") || !strcmp(fileName, ".")) {
+            /*
+                * supress display of ./ ../
+                */
+        } else if (!strncmp(".", fileName, 1)) {
+            /*
+                * suppress display of hidden files
+                */
+        } else {
+            if (idx == 0) {
+                printf("[%d]\t%s\n", idx, "../");
+            }
+            idx++; // start at [1], [0] reserved for prev dir
+            dirStack[idx] = fileName;
+            printf("[%d]\t%s\n", idx, dirStack[idx]);
+        }
+    }
+    closedir(d);
 }
 
 /*
@@ -78,6 +85,9 @@ void read(char* l) {
  */
 char* match(int a) {
     printf("match(%d)\n", a);
+    if (a == 0) {
+        return "../";
+    }
     for (int i = 0; i < sizeof(dirStack); i++) {
         if (i == a) {
             printf("Found %s\n", dirStack[i]);
